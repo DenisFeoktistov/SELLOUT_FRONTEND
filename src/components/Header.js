c790c753639b6d37a52d8e8943c6413e37fde7c7
= <mefeldman_1@edu.hse.ru>
2023-04-06 22:46:06 +0300

    1st commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,34 +2,33 @@ import React from 'react'
 import logo from '../images/header_logo.svg';
 function Header() {
   return (
-    <header class="header">
-    <div class="top">
-        <div class="menu">
-            <p class="menu__element">Women</p>
-            <p class="menu__element">Men</p>
-            <p class="menu__element">Kids</p>
-            <p class="menu__element">Beauty</p>
+    <header className="header">
+    <div className="top">
+        <div className="menu">
+            <p className="menu__element">О нас</p>
+            <p className="menu__element">Блог</p>
         </div>
-        <h1 class="title">SELLOUT</h1>
-        <div class="personal">
+        <h1 className="title">SELLOUT</h1>
+        <div className="personal">
             <p>Личный кабинет</p>
-            <p>Избранное</p>
+            <img src="../images/favourites_unfilled.svg" alt="Heart Image"/>
             <p>Корзина</p>
         </div>
     </div>
-    <div class="top">
-        <div class="menu">
-            <p class="menu__element">20% Off</p>
-            <p class="menu__element">New in</p>
-            <p class="menu__element">Shop By</p>
-            <p class="menu__element">Brands</p>
-            <p class="menu__element">Clothing</p>
-            <p class="menu__element">Shoes</p>
-            <p class="menu__element">Trainers</p>
+    <div className="top">
+        <div className="menu">
+            <p className="menu__element">Новинки</p>
+            <p className="menu__element">Рекомендации</p>
+            <p className="menu__element">Бренды</p>
+            <p className="menu__element">Обувь</p>
+            <p className="menu__element">Одежда</p>
+            <p className="menu__element">Аксессуары</p>
+            <p className="menu__element">Мгновенная доставка</p>
+            <p className="menu__element">Скидки</p>
         </div>
-        <form class="header__form"> 
-            <input type="text" name="text" class="search" placeholder="Search here!"/>
-            <input type="submit" name="submit" class="submit" value="Search"/>
+        <form className="header__form">
+            <input type="text" name="text" className="search" placeholder="Поиск"/>
+            <input type="submit" name="submit" className="submit" value="Search"/>
           </form>
 
     </div>
